9378c38170310d98a3030efd8d77eb5925189ed1
car06 <221220@ids.upchiapas.edu.mx>
2025-04-21 19:44:50 -0600

    fixing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes, } from '@angular/router';
 import LoginPageComponent from './auth/pages/login-page/login-page.component';
-
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
 import { TestPageComponent } from './test/test-page/test-page.component';
 import { LandingPageComponent } from './landing-feature/pages/landing-page/landing-page.component';
 
